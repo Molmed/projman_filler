7bf0d7e554398f1a5ec359eebdbf8463c255622d
MatildaAslin <matilda.aslin@medsci.uu.se>
2018-02-07 14:11:25 +0100

    Added bcl2fastq_output_dir option

--- projman_filler/cli.py
+++ projman_filler/cli.py
@@ -13,13 +13,14 @@ from projman_filler.exceptions import FlowcellAlreadyInDb
 @click.command("projman_filler")
 @click.option('--force', is_flag=True)
 @click.option('--debug', is_flag=True)
+@click.option('-b', '--bcl2fastq-output-dir', default="Unaligned", type=click.Path())
 @click.argument('runfolder', type=click.Path())
-def main(runfolder, force, debug):
+def main(runfolder, force, bcl2fastq_output_dir, debug):
     """Console script for projman_filler."""
     try:
         db_connection_string = os.environ["PROJMAN_DB"]
         app = App(db_connection_string, debug)
-        app.insert_runfolder_into_db(runfolder, force)
+        app.insert_runfolder_into_db(runfolder, bcl2fastq_output_dir, force)
     except FlowcellAlreadyInDb:
         print("Flowcell was already present in db.")
         sys.exit(1)
