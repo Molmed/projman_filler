1a35214acd5eec9d145c51c75d8f19f643c63ffd
MatildaAslin <matilda.aslin@medsci.uu.se>
2018-03-28 17:01:42 +0200

    Added new test for multiple SampleIDs per sample

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -724,3 +724,530 @@ conversion_results_sample_with_no_reads = [
         }
     }
 ]
+
+conversion_results_multiple_sampleIDs_per_sampleName = [
+    {
+        "LaneNumber": 1,
+        "TotalClustersRaw": 103535184,
+        "TotalClustersPF": 38589884,
+        "Yield": 11654144968,
+        "DemuxResults": [
+            {
+                "SampleId": "SI-GA-D1_1",
+                "SampleName": "A",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "CACTCGGA",
+                        "MismatchCounts": {
+                            "0": 2274651,
+                            "1": 85470
+                        }
+                    }
+                ],
+                "NumberReads": 2360121,
+                "Yield": 712756542,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 356378271,
+                        "YieldQ30": 329483985,
+                        "QualityScoreSum": 13957005593,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 356378271,
+                        "YieldQ30": 299507068,
+                        "QualityScoreSum": 13028886790,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-D1_2",
+                "SampleName": "A",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "GCTGAATT",
+                        "MismatchCounts": {
+                            "0": 2139332,
+                            "1": 82667
+                        }
+                    }
+                ],
+                "NumberReads": 2221999,
+                "Yield": 671043698,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 335521849,
+                        "YieldQ30": 310862353,
+                        "QualityScoreSum": 13163037394,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 335521849,
+                        "YieldQ30": 283962518,
+                        "QualityScoreSum": 12330016516,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-D1_3",
+                "SampleName": "A",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "TGAAGTAC",
+                        "MismatchCounts": {
+                            "0": 1930149,
+                            "1": 71556
+                        }
+                    }
+                ],
+                "NumberReads": 2001705,
+                "Yield": 604514910,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 302257455,
+                        "YieldQ30": 279609899,
+                        "QualityScoreSum": 11843970369,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 302257455,
+                        "YieldQ30": 255874175,
+                        "QualityScoreSum": 11110175116,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-D1_4",
+                "SampleName": "A",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "ATGCTCCG",
+                        "MismatchCounts": {
+                            "0": 2306340,
+                            "1": 86685
+                        }
+                    }
+                ],
+                "NumberReads": 2393025,
+                "Yield": 722693550,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 361346775,
+                        "YieldQ30": 334261695,
+                        "QualityScoreSum": 14156351488,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 361346775,
+                        "YieldQ30": 303964312,
+                        "QualityScoreSum": 13218264706,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-E1_1",
+                "SampleName": "B",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "TGGTAAAC",
+                        "MismatchCounts": {
+                            "0": 1143003,
+                            "1": 41386
+                        }
+                    }
+                ],
+                "NumberReads": 1184389,
+                "Yield": 357685478,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 178842739,
+                        "YieldQ30": 165385176,
+                        "QualityScoreSum": 7006747411,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 178842739,
+                        "YieldQ30": 150969608,
+                        "QualityScoreSum": 6565240826,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-E1_2",
+                "SampleName": "B",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "GAAAGGGT",
+                        "MismatchCounts": {
+                            "0": 916718,
+                            "1": 42405
+                        }
+                    }
+                ],
+                "NumberReads": 959123,
+                "Yield": 289655146,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 144827573,
+                        "YieldQ30": 134206272,
+                        "QualityScoreSum": 5682054371,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 144827573,
+                        "YieldQ30": 122410512,
+                        "QualityScoreSum": 5320838401,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-E1_3",
+                "SampleName": "B",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "ACTGCTCG",
+                        "MismatchCounts": {
+                            "0": 1135521,
+                            "1": 42244
+                        }
+                    }
+                ],
+                "NumberReads": 1177765,
+                "Yield": 355685030,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 177842515,
+                        "YieldQ30": 164528051,
+                        "QualityScoreSum": 6968516555,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 177842515,
+                        "YieldQ30": 150115220,
+                        "QualityScoreSum": 6526537022,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-E1_4",
+                "SampleName": "B",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "CTCCTCTA",
+                        "MismatchCounts": {
+                            "0": 961404,
+                            "1": 37648
+                        }
+                    }
+                ],
+                "NumberReads": 999052,
+                "Yield": 301713704,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 150856852,
+                        "YieldQ30": 139411820,
+                        "QualityScoreSum": 5906840307,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 150856852,
+                        "YieldQ30": 127197470,
+                        "QualityScoreSum": 5532470723,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-F1_1",
+                "SampleName": "C",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "GTTGCAGC",
+                        "MismatchCounts": {
+                            "0": 1108530,
+                            "1": 38540
+                        }
+                    }
+                ],
+                "NumberReads": 1147070,
+                "Yield": 346415140,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 173207570,
+                        "YieldQ30": 162593999,
+                        "QualityScoreSum": 6859274749,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 173207570,
+                        "YieldQ30": 149339768,
+                        "QualityScoreSum": 6449124698,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-F1_2",
+                "SampleName": "C",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "TGGAATTA",
+                        "MismatchCounts": {
+                            "0": 911187,
+                            "1": 34194
+                        }
+                    }
+                ],
+                "NumberReads": 945381,
+                "Yield": 285505062,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 142752531,
+                        "YieldQ30": 134198097,
+                        "QualityScoreSum": 5660208120,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 142752531,
+                        "YieldQ30": 123994992,
+                        "QualityScoreSum": 5344773746,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-F1_3",
+                "SampleName": "C",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "CAATGGAG",
+                        "MismatchCounts": {
+                            "0": 852607,
+                            "1": 43965
+                        }
+                    }
+                ],
+                "NumberReads": 896572,
+                "Yield": 270764744,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 135382372,
+                        "YieldQ30": 127320977,
+                        "QualityScoreSum": 5369242443,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 135382372,
+                        "YieldQ30": 117165952,
+                        "QualityScoreSum": 5054745240,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-F1_4",
+                "SampleName": "C",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "ACCCTCCT",
+                        "MismatchCounts": {
+                            "0": 581426,
+                            "1": 24586
+                        }
+                    }
+                ],
+                "NumberReads": 606012,
+                "Yield": 183015624,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 91507812,
+                        "YieldQ30": 85926425,
+                        "QualityScoreSum": 3624782594,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 91507812,
+                        "YieldQ30": 79151231,
+                        "QualityScoreSum": 3414687235,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-F2_1",
+                "SampleName": "D",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "TTTACATG",
+                        "MismatchCounts": {
+                            "0": 5787067,
+                            "1": 200245
+                        }
+                    }
+                ],
+                "NumberReads": 5987312,
+                "Yield": 1808168224,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 904084112,
+                        "YieldQ30": 852053063,
+                        "QualityScoreSum": 35915322386,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 904084112,
+                        "YieldQ30": 780763060,
+                        "QualityScoreSum": 33691837925,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-F2_2",
+                "SampleName": "D",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "CGCGATAC",
+                        "MismatchCounts": {
+                            "0": 6232031,
+                            "1": 217175
+                        }
+                    }
+                ],
+                "NumberReads": 6449206,
+                "Yield": 1947660212,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 973830106,
+                        "YieldQ30": 916714197,
+                        "QualityScoreSum": 38646385421,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 973830106,
+                        "YieldQ30": 840388331,
+                        "QualityScoreSum": 36268828675,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-F2_3",
+                "SampleName": "D",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "ACGCGGGT",
+                        "MismatchCounts": {
+                            "0": 4609609,
+                            "1": 198043
+                        }
+                    }
+                ],
+                "NumberReads": 4807652,
+                "Yield": 1451910904,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 725955452,
+                        "YieldQ30": 683571059,
+                        "QualityScoreSum": 28813256864,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 725955452,
+                        "YieldQ30": 624087820,
+                        "QualityScoreSum": 26960063425,
+                        "TrimmedBases": 0
+                    }
+                ]
+            },
+            {
+                "SampleId": "SI-GA-F2_4",
+                "SampleName": "D",
+                "IndexMetrics": [
+                    {
+                        "IndexSequence": "GAATTCCA",
+                        "MismatchCounts": {
+                            "0": 2928545,
+                            "1": 108803
+                        }
+                    }
+                ],
+                "NumberReads": 3037348,
+                "Yield": 917279096,
+                "ReadMetrics": [
+                    {
+                        "ReadNumber": 1,
+                        "Yield": 458639548,
+                        "YieldQ30": 432097908,
+                        "QualityScoreSum": 18213911955,
+                        "TrimmedBases": 0
+                    },
+                    {
+                        "ReadNumber": 2,
+                        "Yield": 458639548,
+                        "YieldQ30": 396946240,
+                        "QualityScoreSum": 17121530276,
+                        "TrimmedBases": 0
+                    }
+                ]
+            }
+        ],
+        "Undetermined": {
+            "NumberReads": 1416152,
+            "Yield": 427677904,
+            "ReadMetrics": [
+                {
+                    "ReadNumber": 1,
+                    "Yield": 213838952,
+                    "YieldQ30": 192146394,
+                    "QualityScoreSum": 8196259529,
+                    "TrimmedBases": 0
+                },
+                {
+                    "ReadNumber": 2,
+                    "Yield": 213838952,
+                    "YieldQ30": 173195437,
+                    "QualityScoreSum": 7625129659,
+                    "TrimmedBases": 0
+                }
+            ]
+        }
+    }
+]
