dbaf1c52e057f1e88972f2cd79d539747ab9b682
MatildaAslin <matilda.aslin@medsci.uu.se>
2018-04-10 16:55:17 +0200

    Add test case for empty lane

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1251,3 +1251,228 @@ conversion_results_multiple_sampleIDs_per_sampleName = [
         }
     }
 ]
+
+conversion_results_empty_lane = [
+    {
+        "LaneNumber" : 1,
+        "TotalClustersRaw" : 168865204,
+        "TotalClustersPF" : 0,
+        "Yield" : 0,
+        "DemuxResults" : [
+            {
+                "SampleId" : "Sample_A",
+                "SampleName" : "A",
+                "IndexMetrics" : [
+                    {
+                        "IndexSequence" : "GTAGAGGA+CTCTCTAT",
+                        "MismatchCounts" : {
+                            "0" : 0,
+                            "1" : 0
+                        }
+                    }
+                ],
+                "NumberReads" : 0,
+                "Yield" : 0,
+                "ReadMetrics" : [
+                    {
+                        "ReadNumber" : 1,
+                        "Yield" : 0,
+                        "YieldQ30" : 0,
+                        "QualityScoreSum" : 0,
+                        "TrimmedBases" : 0
+                    },
+                    {
+                        "ReadNumber" : 2,
+                        "Yield" : 0,
+                        "YieldQ30" : 0,
+                        "QualityScoreSum" : 0,
+                        "TrimmedBases" : 0
+                    }
+                ]
+            },
+            {
+                "SampleId" : "Sample_B",
+                "SampleName" : "B",
+                "IndexMetrics" : [
+                    {
+                        "IndexSequence" : "AGGCAGAA+TAGATCGC",
+                        "MismatchCounts" : {
+                            "0" : 0,
+                            "1" : 0
+                        }
+                    },
+                    {
+                        "IndexSequence" : "CGTACTAG+TAGATCGC",
+                        "MismatchCounts" : {
+                            "0" : 0,
+                            "1" : 0
+                        }
+                    },
+                    {
+                        "IndexSequence" : "TAAGGCGA+TAGATCGC",
+                        "MismatchCounts" : {
+                            "0" : 0,
+                            "1" : 0
+                        }
+                    }
+                ],
+                "NumberReads" : 0,
+                "Yield" : 0,
+                "ReadMetrics" : [
+                    {
+                        "ReadNumber" : 1,
+                        "Yield" : 0,
+                        "YieldQ30" : 0,
+                        "QualityScoreSum" : 0,
+                        "TrimmedBases" : 0
+                    },
+                    {
+                        "ReadNumber" : 2,
+                        "Yield" : 0,
+                        "YieldQ30" : 0,
+                        "QualityScoreSum" : 0,
+                        "TrimmedBases" : 0
+                    }
+                ]
+            }
+        ],
+        "Undetermined" : {
+            "NumberReads" : 0,
+            "Yield" : 0,
+            "ReadMetrics" : [
+                {
+                    "ReadNumber" : 1,
+                    "Yield" : 0,
+                    "YieldQ30" : 0,
+                    "QualityScoreSum" : 0,
+                    "TrimmedBases" : 0
+                },
+                {
+                    "ReadNumber" : 2,
+                    "Yield" : 0,
+                    "YieldQ30" : 0,
+                    "QualityScoreSum" : 0,
+                    "TrimmedBases" : 0
+                }
+            ]
+        }
+    },
+    {
+        "LaneNumber" : 2,
+        "TotalClustersRaw" : 170966905,
+        "TotalClustersPF" : 164470667,
+        "Yield" : 33223074734,
+        "DemuxResults" : [
+            {
+                "SampleId" : "Sample_C",
+                "SampleName" : "C",
+                "IndexMetrics" : [
+                    {
+                        "IndexSequence" : "GTAGAGGA+CTCTCTAT",
+                        "MismatchCounts" : {
+                            "0" : 25089241,
+                            "1" : 537794
+                        }
+                    },
+                    {
+                        "IndexSequence" : "TAGGCATG+CTCTCTAT",
+                        "MismatchCounts" : {
+                            "0" : 28780128,
+                            "1" : 635590
+                        }
+                    },
+                    {
+                        "IndexSequence" : "TCCTGAGC+CTCTCTAT",
+                        "MismatchCounts" : {
+                            "0" : 26448410,
+                            "1" : 621493
+                        }
+                    }
+                ],
+                "NumberReads" : 82112656,
+                "Yield" : 16586756512,
+                "ReadMetrics" : [
+                    {
+                        "ReadNumber" : 1,
+                        "Yield" : 8293378256,
+                        "YieldQ30" : 8124748274,
+                        "QualityScoreSum" : 321961230718,
+                        "TrimmedBases" : 0
+                    },
+                    {
+                        "ReadNumber" : 2,
+                        "Yield" : 8293378256,
+                        "YieldQ30" : 7986468752,
+                        "QualityScoreSum" : 317888157381,
+                        "TrimmedBases" : 0
+                    }
+                ]
+            },
+            {
+                "SampleId" : "Sample_D",
+                "SampleName" : "D",
+                "IndexMetrics" : [
+                    {
+                        "IndexSequence" : "AGGCAGAA+TAGATCGC",
+                        "MismatchCounts" : {
+                            "0" : 21699712,
+                            "1" : 470888
+                        }
+                    },
+                    {
+                        "IndexSequence" : "CGTACTAG+TAGATCGC",
+                        "MismatchCounts" : {
+                            "0" : 29574087,
+                            "1" : 680613
+                        }
+                    },
+                    {
+                        "IndexSequence" : "TAAGGCGA+TAGATCGC",
+                        "MismatchCounts" : {
+                            "0" : 24687217,
+                            "1" : 525973
+                        }
+                    }
+                ],
+                "NumberReads" : 77638490,
+                "Yield" : 15682974980,
+                "ReadMetrics" : [
+                    {
+                        "ReadNumber" : 1,
+                        "Yield" : 7841487490,
+                        "YieldQ30" : 7681071746,
+                        "QualityScoreSum" : 304372390400,
+                        "TrimmedBases" : 0
+                    },
+                    {
+                        "ReadNumber" : 2,
+                        "Yield" : 7841487490,
+                        "YieldQ30" : 7545152543,
+                        "QualityScoreSum" : 300239296094,
+                        "TrimmedBases" : 0
+                    }
+                ]
+            }
+        ],
+        "Undetermined" : {
+            "NumberReads" : 4719521,
+            "Yield" : 953343242,
+            "ReadMetrics" : [
+                {
+                    "ReadNumber" : 1,
+                    "Yield" : 476671621,
+                    "YieldQ30" : 462903475,
+                    "QualityScoreSum" : 18405105508,
+                    "TrimmedBases" : 0
+                },
+                {
+                    "ReadNumber" : 2,
+                    "Yield" : 476671621,
+                    "YieldQ30" : 450651308,
+                    "QualityScoreSum" : 18081960163,
+                    "TrimmedBases" : 0
+                }
+            ]
+        }
+    }
+]
