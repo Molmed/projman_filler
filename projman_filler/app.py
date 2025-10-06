d5d1807670a5bd8fe967f31ac7181920a2029a17
nelnk861 <nelly.nkonge@uu.se>
2025-10-06 14:34:31 +0200

    Refactored code and added class to handle checkQC parser results

--- projman_filler/app.py
+++ projman_filler/app.py
@@ -11,6 +11,7 @@ from projman_filler.exceptions import FlowcellAlreadyInDb
 from projman_filler.models.db_models import FlowcellRunfolder
 from projman_filler.bcl2fastq_run_stats_parser import Bcl2fastqRunStatsParser
 from projman_filler.interop_run_stats_parser import InteropRunStatsParser
+from projman_filler.qc_data_parser import QCDataParser
 
 from projman_filler.repositories.sample_results_repo import SampleResultRepo
 from projman_filler.repositories.flowcell_lane_results_repo import FlowcellLaneResultsRepo
@@ -44,8 +45,8 @@ class App(object):
             return
 
         if force:
-            print("Found the specified runfolder in the db, but got a force " \
-            "option, so will proceed to delete it and insert new values.")
+            print("Found the specified runfolder in the db, but got a force "
+                "option, so will proceed to delete it and insert new values.")
             self.flowcell_lane_results_repo.delete_by_flowcell_name(flowcell_name)
             self.flowcell_runfolder_repo.delete_by_flowcell_name(flowcell_name)
             self.sample_results_repo.delete_by_flowcell_name(flowcell_name)
@@ -60,8 +61,8 @@ class App(object):
                                                run_date=runfolder_date)
         self.flowcell_runfolder_repo.add(flowcell_runfolder)
 
-    def insert_runfolder_into_db(self, runfolder, bcl2fastq_stats_dir, \
-                                 demultiplexer, config, force=False, \
+    def insert_runfolder_into_db(self, runfolder, bcl2fastq_stats_dir,
+                                 demultiplexer, ch_config_path, force=False,
                                  atac_seq_mode=False, olink_mode=False):
         """
         Inserts runfolder data into the specific database based on the specified 
@@ -69,8 +70,8 @@ class App(object):
 
         :param runfolder (str): Path to the runfolder directory.
         :param bcl2fastq_stats_dir (str): Subdirectory containing bcl2fastq statistics.
-        :param demultiplexer (str): Demultiplexer used (e.g 'bcl2fastq'/ 'bclconvert'...).
-        :param config (Path): Path to the checkQC configuration file
+        :param demultiplexer (str): Demultiplexer used (e.g 'bcl2fastq' or 'bclconvert').
+        :param ch_config_path (Path): Path to the checkQC configuration file
         :param force (bool, optional): If True, existing flowcell data will be overwritten.
         :param atac_seq_mode (bool, optional): If True, enables ATAC-seq specific processing.
         :param olink_mode (bool, optional): If True, enables Olink-specific processing.
@@ -86,8 +87,8 @@ class App(object):
                   "InterOp files instead of bcl2fastq Stats.json.")
             return self.insert_olink_runfolder_into_db(runfolder, force)
         else:
-           flowcell_name = self._handle_other_demultiplexer(
-               runfolder, demultiplexer, config, force
+            flowcell_name = self._handle_other_demultiplexer(
+               runfolder, demultiplexer, ch_config_path, force
             )
 
         self.insert_flowcell_runfolder_into_db(runfolder, flowcell_name)
@@ -155,33 +156,35 @@ class App(object):
         self.flowcell_lane_results_repo.add(list(lane_stats))
         self.insert_flowcell_runfolder_into_db(runfolder, flowcell_name)
 
-    def _handle_other_demultiplexer(self, runfolder, demultiplexer, config, force):
+    def _handle_other_demultiplexer(self, runfolder, demultiplexer, ch_config_path,
+                                     force):
         """
         Handles runfolder processing for demultiplexers other than bcl2fastq.
 
         :param runfolder (str): Path to the runfolder directory.
         :param demultiplexer (str): Demultiplexer used (e.g., 'bclconvert').
-        :param config (Path): Path to the checkQC configuration file
+        :param ch_config_path (Path): Path to the checkQC configuration file
         :param force (bool): If True, existing flowcell data will be overwritten.
 
         :return flowcell_name (str): The flowcell name extracted from the runfolder 
             (after saving flowcel_lane and sample data in DB).
         """
 
-        interop = InteropRunStatsParser(runfolder)
-        config = ConfigFactory.from_config_path(config)._config
+        checkqc_conf = ConfigFactory.from_config_path(ch_config_path)._config
         qc_data = getattr(QCData, f"from_{demultiplexer}")(
             runfolder_path=runfolder,
             parser_config=(
-                config
-            .get("parser_configurations", {})
-            .get(f"from_{demultiplexer}", {})
+                checkqc_conf
+                .get("parser_configurations", {})
+                .get(f"from_{demultiplexer}", {})
             )
         )
-        flowcell_name = interop.get_flowcell_name()
+        qc_data_parser = QCDataParser(qc_data, runfolder)
+        flowcell_name = qc_data_parser.flowcell_id
         self.delete_existing_flowcell_from_db(flowcell_name, force)
 
-        lane_results, sample_results = interop.get_checkqc_interop_stats(qc_data)
+        lane_results = qc_data_parser._build_lane_results()
+        sample_results = qc_data_parser._build_sample_results()
         self.flowcell_lane_results_repo.add(lane_results)
         self.sample_results_repo.add(sample_results)
 
